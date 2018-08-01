e3a75227fed58748eb2adae256e44a02ac8eebe1
fmn <cdnxyf@gmail.com>
2018-08-01 22:52:18 +0800

    first commit

---
+++ src/main/java/com/ibing/ibing/IbingApplication.java
@@ -0,0 +1,12 @@
+package com.ibing.ibing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ibing/ibing/IbingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibing.ibing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IbingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
